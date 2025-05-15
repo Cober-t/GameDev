32391056915a8fd38cd696480a8493e3c93d1d6a
jorge <jorgeouscober@gmail.com>
2025-05-16 01:06:50 +0200

    Fixed jitering camera smooth position

--- Above&Beyond/scripts/player_movement.gd
+++ Above&Beyond/scripts/player_movement.gd
@@ -35,7 +35,6 @@ var sprite_offset: Vector2 = Vector2.ZERO
 var last_direction_pressed: int = 1
 
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var camera_2d: Camera2D = $"../Camera2D"
 
 
 # ---------------------------------------------------------------------
@@ -62,16 +61,15 @@ func fix_movement_jittering():
 	
 	# Optionally update camera position if it's a child of this node
 	#if has_node("Camera2D"):
-	#camera_2d.global_position = render_position
+		#camera_2d.global_position = render_position
 
 # ---------------------------------------------------------------------
 
 func _process(delta: float) -> void:
-	pass
 	fix_movement_jittering()
 	
 # ---------------------------------------------------------------------
-
+#
 func _physics_process(delta):
 	
 	previous_position = global_position
