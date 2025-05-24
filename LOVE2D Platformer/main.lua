d5d8850a930ca0c9bcaea8a102afbb86397cba6b
Cober-t <jorgeouscober@gmail.com>
2025-05-24 17:17:29 +0200

    Tiled level importer and camera

--- LOVE2D Platformer/main.lua
+++ LOVE2D Platformer/main.lua
@@ -2,16 +2,24 @@ _G.love = require("love")
 
 player = {}
 function love.load()
-    anim8 = require 'libraries/anim8/anim8'
     love.graphics.setDefaultFilter("nearest", "nearest")
-    -- anim8 = require 'libraries/tiled/tield'
+    love.physics.setMeter(32)
+    world = love.physics.newWorld(0, 0)
+    anim8 = require 'libraries/anim8/anim8' -- Animation utility
+    sti = require 'libraries/sti'           -- Level importer from Tiled
+    humCamera = require 'libraries/hump/camera'-- Camera utility from HUMP
+    camera = humCamera()
+
+    gameMap = sti("assets/maps/testLevel.lua", { "box2d" })
+    gameMap:box2d_init(world)
+
     player:initAnimations()
-    background = love.graphics.newImage("assets/sprites/world_tileset.png")
+    -- background = love.graphics.newImage("assets/sprites/world_tileset.png")
 end
 
 function player:initAnimations()
-    self.x = 50
-    self.y = 50
+    self.x = 0
+    self.y = 0
     self.direction = 1
     self.speed = 200.0
     self.spriteSheet = love.graphics.newImage("assets/sprites/knight.png")
@@ -24,6 +32,7 @@ function player:initAnimations()
     self.animations.jumpRight = anim8.newAnimation(self.grid(4, 6), animSpeed)
     self.animations.idleLeft = self.animations.idleRight:clone():flipH()
     self.animations.walkLeft = self.animations.walkRight:clone():flipH()
+    self.animations.jumpLeft = self.animations.jumpRight:clone():flipH()
 end
 
 
@@ -37,6 +46,9 @@ function love.update(dt)
         player.x = player.x + dt * player.speed
         player.direction = 1
         state = "walkRight"
+    -- elseif love.keyboard.isDown("space") then
+    --     player.y = player.y + dt * player.speed
+    --     if player.direction == -1 then state = "jumpLeft" else state = "jumpRight" end
     elseif love.keyboard.isDown("escape") then
         love.event.quit()
     elseif player.direction == -1 then state = "idleLeft" else state = "idleRight" end
@@ -44,10 +56,36 @@ function love.update(dt)
     player.state = state
     player.animation = player.animations[player.state]
     player.animation:update(dt)
+
+    local leftLimit = love.graphics.getWidth()
+    local topLimit = love.graphics.getHeight()
+    local rightLimit = gameMap.width * gameMap.tilewidth
+    local bottomLimit = gameMap.height * gameMap.tileheight
+    -- camera:lookAt(player.x, player.y - bottomLimit - topLimit/2)
+    camera:lookAt(player.x, player.y)
+
+    if camera.x < leftLimit / 2 then
+        camera.x = leftLimit /2
+    end
+    if camera.y < topLimit / 2 then
+        camera.y = topLimit / 2
+    end
+    if camera.x > rightLimit - leftLimit / 2 then
+        camera.x = rightLimit - leftLimit /2
+    end
+    if camera.y > bottomLimit - topLimit / 2 then
+        camera.y = bottomLimit - topLimit /2
+    end
 end
 
 function love.draw()
     local scale = 5
+    camera:attach()
+        gameMap:drawLayer(gameMap.layers["Background"])
+        gameMap:drawLayer(gameMap.layers["World"])
+        player.animation:draw(player.spriteSheet, player.x, player.y, nil, scale, scale, 8, 8)
+    camera:detach()
+
+    -- Draw HUD
     love.graphics.print(player.state)
-    player.animation:draw(player.spriteSheet, player.x, player.y, nil, scale, scale)
 end
